31069fb588c72522135bfa3ca167b693b6e186fd
RizvanVeliyev <veliyevrizvan250@gmail.com>
2022-12-05 08:59:31 +0400

    first commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿namespace ConsoleApp2
+﻿namespace github1
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Console.Write("hello world");
 		}
 	}
 }
